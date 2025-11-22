c7f34a5c19a2d145b436044a669b6c0424058b5f
Gemarcx <gerardbenetmartinez@gmail.com>
2025-11-22 15:00:31 +0100

    Hotfix model deletion

--- src/modelManager.cpp
+++ src/modelManager.cpp
@@ -20,9 +20,11 @@ modelManager::~modelManager()
 {
 	delete m_modelMapa;
 	for (COBJModel* m : m_modelsEnemics)
-		if(m!=nullptr) 
+		if (m != nullptr) 
+		{
 			delete m;
-
+			m = nullptr;
+		}
 }
 
 void modelManager::initialSetup()
